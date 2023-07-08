7cce21662d1cf136d2579a986e0b2fec5046ec13
IIHARA HODAKA <iihara.h@xscore.co.jp>
2023-07-08 10:51:45 +0900

    [chore] update settings

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -2,8 +2,7 @@ module.exports = {
   customSyntax: 'postcss-html',
   extends: [
     'stylelint-config-standard-scss',
-    'stylelint-config-recess-order',
-    'stylelint-config-prettier'
+    'stylelint-config-recess-order'
   ],
   plugins: [],
   ignoreFiles: ['**/node_modules/**', '**/.nuxt/**', '**/dist/**'],
